50430b7ee7034cc7bbb628759a314a0e495b05d5
Thomas PRIOUL (MASTERIS) <thomas.prioul@masteris.com>
2025-06-05 09:58:14 +0000

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/zGGtQFVQXR

---
+++ apps/mdl-angular-libs/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  displayName: 'mdl-angular-libs',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/apps/mdl-angular-libs',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
