8771e19795bd6a381e7400d9476eb5b057842ce1
Thomas PRIOUL (MASTERIS) <thomas.prioul@masteris.com>
2025-08-11 14:58:10 +0000

    WIP add CSS automatic build with tailwind + daisyUI

--- libs/mdl-angular/ui-timewindow/src/index.ts
+++ libs/mdl-angular/ui-timewindow/src/index.ts
@@ -1,5 +1,8 @@
+import './lib/styles/generated.css';
+
 export * from './lib/calendar';
 export * from './lib/i18n';
 export * from './lib/model';
 export * from './lib/timewindow';
 export * from './lib/timewindow-popup';
+
